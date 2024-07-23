e5601a8d53a24ce3013bc36cda2d9ffd9eb404be
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-07-23 21:24:28 +0900

    refactor : #4 클래스 오타명 수정

--- escape-core/src/main/java/com/sparta/global/exception/customException/ReactionException.java
+++ escape-core/src/main/java/com/sparta/global/exception/customException/ReactionException.java
@@ -2,8 +2,8 @@ package com.sparta.global.exception.customException;
 
 import com.sparta.global.exception.errorCode.ErrorCode;
 
-public class ReactionExcepiton extends GlobalCustomException {
-    public ReactionExcepiton(ErrorCode errorCode) {
+public class ReactionException extends GlobalCustomException {
+    public ReactionException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
