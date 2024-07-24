e8f80020d7210dbd62342c1364f37c2ed7d1e99b
backendINFJ <backendINFJ@gmail.com>
2024-07-24 11:04:33 +0900

    chore: develop merge 하면서 누락된 코드 추가 (UserRepository,UserErrorCode)

--- escape-core/src/main/java/com/sparta/domain/user/repository/UserRepository.java
+++ escape-core/src/main/java/com/sparta/domain/user/repository/UserRepository.java
@@ -1,11 +1,20 @@
 package com.sparta.domain.user.repository;
 
 import com.sparta.domain.user.entity.User;
+import com.sparta.domain.user.entity.UserType;
+import com.sparta.global.exception.customException.UserException;
+import com.sparta.global.exception.errorCode.UserErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User findByIdOrElseThrow(Long userId) {
+        return findById(userId).orElseThrow(()->new UserException(UserErrorCode.USER_NOT_FOUND));
+    }
+    List<User> findByUserType(UserType userType);
 
     Optional<User> findByEmail(String email); // 고민중
 }
--- escape-core/src/main/java/com/sparta/global/exception/errorCode/UserErrorCode.java
+++ escape-core/src/main/java/com/sparta/global/exception/errorCode/UserErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
+    USER_NOT_AUTHORITY(HttpStatus.UNAUTHORIZED.value(),"접근 권한이 없습니다." ),
+    USER_NO_ONE(HttpStatus.NOT_FOUND.value(),"유저가 존재하지 않습니다." ),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 유저를 찾을 수 없습니다."),
     USER_DUPLICATION(HttpStatus.BAD_REQUEST.value(), "이미 등록된 이메일 입니다."),
     USER_WITHDRAW(HttpStatus.BAD_REQUEST.value(), "해당 사용자는 탈퇴한 사용자입니다.");
