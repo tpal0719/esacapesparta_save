937c305c84fa6ea7a7e696009afebd28b8d8f333
backendINFJ <backendINFJ@gmail.com>
2024-07-25 01:45:21 +0900

    chore : merge전 코드 더블체크

--- escape-core/src/main/java/com/sparta/security/UserDetailsImpl.java
+++ escape-core/src/main/java/com/sparta/security/UserDetailsImpl.java
@@ -15,6 +15,10 @@ public class UserDetailsImpl implements UserDetails {
 
     private final User user;
 
+    public String getUserEmail() {
+        return user.getEmail();
+    }
+
     public UserDetailsImpl(User user) {
         this.user = user;
     }
