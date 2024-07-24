c84978fea781e495e5ae7bee525b938551b02611
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-07-24 09:12:22 +0900

    fix : #4 병합 오타 수정

--- escape-core/src/main/java/com/sparta/domain/store/repository/StoreRepository.java
+++ escape-core/src/main/java/com/sparta/domain/store/repository/StoreRepository.java
@@ -5,7 +5,7 @@ import com.sparta.global.exception.customException.StoreException;
 import com.sparta.global.exception.errorCode.StoreErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
+public interface StoreRepository extends JpaRepository<Store, Long> , StoreRepositoryCustom{
     default Store findByIdOrElseThrow(Long storeId){
         return findById(storeId).orElseThrow(
                 ()-> new StoreException(StoreErrorCode.STORE_NOT_FOUND)
