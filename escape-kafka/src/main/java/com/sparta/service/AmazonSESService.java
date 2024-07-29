79ba9da1adb2d776cf18835883e24bf981851675
seongkyun <yooss135@naver.com>
2024-07-29 21:50:54 +0900

    fix : #52 push 오류 해결

---
+++ escape-kafka/src/main/java/com/sparta/service/AmazonSESService.java
@@ -0,0 +1,27 @@
+package com.sparta.service;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AmazonSESService {
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    public void sendEmail(String email, String subject, String body) {
+        log.error("5");
+        SendEmailRequest request = new SendEmailRequest()
+                .withDestination(new Destination().withToAddresses(email))
+                .withMessage(new Message()
+                        .withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData(body)))
+                        .withSubject(new Content().withCharset("UTF-8").withData(subject)))
+                .withSource(email);
+        log.error("6");
+        amazonSimpleEmailService.sendEmail(request);
+    }
+
+}
