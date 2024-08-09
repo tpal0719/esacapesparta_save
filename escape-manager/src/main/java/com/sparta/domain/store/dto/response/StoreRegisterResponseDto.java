9623a18c4545b587c1bed568e4d33ecefba833a5
semdev <tpal071997@gmail.com>
2024-08-09 16:22:46 +0900

    refactor : #97 - response final

--- escape-manager/src/main/java/com/sparta/domain/store/dto/response/StoreRegisterResponseDto.java
+++ escape-manager/src/main/java/com/sparta/domain/store/dto/response/StoreRegisterResponseDto.java
@@ -3,40 +3,40 @@ package com.sparta.domain.store.dto.response;
 import com.sparta.domain.store.entity.Store;
 import com.sparta.domain.store.entity.StoreRegion;
 import com.sparta.domain.store.entity.StoreStatus;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
+import lombok.Getter;
 
 @Getter
 public class StoreRegisterResponseDto {
-    private Long storeId;
-    private String name;
-    private String address;
-    private String phoneNumber;
-    private String workHours;
-    private String storeImage;
-    private StoreRegion storeRegion;
-    private StoreStatus storeStatus;
-    private Long managerId;
-    private String managerName;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+
+  private final Long storeId;
+  private final String name;
+  private final String address;
+  private final String phoneNumber;
+  private final String workHours;
+  private final String storeImage;
+  private final StoreRegion storeRegion;
+  private final StoreStatus storeStatus;
+  private final Long managerId;
+  private final String managerName;
+  private final LocalDateTime createdAt;
+  private final LocalDateTime updatedAt;
 
 
-    public StoreRegisterResponseDto(Store store) {
-        this.storeId = store.getId();
-        this.name = store.getName();
-        this.address = store.getAddress();
-        this.phoneNumber = store.getPhoneNumber();
-        this.workHours = store.getWorkHours();
-        this.storeImage = store.getStoreImage();
-        this.storeRegion = store.getStoreRegion();
-        this.storeStatus = store.getStoreStatus();
-        this.managerId = store.getManager().getId();
-        this.managerName = store.getManager().getName();
-        this.createdAt = store.getCreatedAt();
-        this.updatedAt = store.getUpdatedAt();
-    }
+  public StoreRegisterResponseDto(Store store) {
+    this.storeId = store.getId();
+    this.name = store.getName();
+    this.address = store.getAddress();
+    this.phoneNumber = store.getPhoneNumber();
+    this.workHours = store.getWorkHours();
+    this.storeImage = store.getStoreImage();
+    this.storeRegion = store.getStoreRegion();
+    this.storeStatus = store.getStoreStatus();
+    this.managerId = store.getManager().getId();
+    this.managerName = store.getManager().getName();
+    this.createdAt = store.getCreatedAt();
+    this.updatedAt = store.getUpdatedAt();
+  }
 
 //    public static StoreCreateResponseDto of(Store store) {
 //        return StoreCreateResponseDto.builder()
