91855d6b7aee98cd22f00f15115d23f33e9b52d3
semdev <tpal071997@gmail.com>
2024-08-12 23:23:22 +0900

    feature : #53 cicd restore webconfig

--- escape-core/src/main/java/com/sparta/config/WebConfig.java
+++ escape-core/src/main/java/com/sparta/config/WebConfig.java
@@ -4,17 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//  @Override
-//  public void addCorsMappings(CorsRegistry registry) {
-//    registry.addMapping("/**")
-//        .allowedOrigins("*")
-//        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-//        .allowedHeaders("Authorization", "Content-Type")
-//        .exposedHeaders("Custom-Header")
-//        .allowedHeaders("*")
-//        .allowCredentials(true);
-//  }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://escapesparta.store") // 특정 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
