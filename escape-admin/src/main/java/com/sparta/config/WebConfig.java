e87f6739f8c4b92e70b213a2c2382557f1a0b7cb
tichall <0817frog@gmail.com>
2024-08-12 22:26:53 +0900

    fix : #53 WebConfig 주석 처리

--- escape-admin/src/main/java/com/sparta/config/WebConfig.java
+++ escape-admin/src/main/java/com/sparta/config/WebConfig.java
@@ -1,24 +1,24 @@
-package com.sparta.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-  @Value("${base.url}")
-  public String BASE_URL;
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-        .allowedHeaders("Authorization", "Content-Type")
-        .exposedHeaders("Custom-Header")
-        .allowedHeaders("*")
-        .allowCredentials(true);
-  }
-}
+//package com.sparta.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//  @Value("${base.url}")
+//  public String BASE_URL;
+//
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//        .allowedOrigins("*")
+//        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+//        .allowedHeaders("Authorization", "Content-Type")
+//        .exposedHeaders("Custom-Header")
+//        .allowedHeaders("*")
+//        .allowCredentials(true);
+//  }
+//}
